50bbd7529f40e8dbef87af892cb3955bcd86174d
muzimzz <rlagusdn0914@naver.com>
2026-04-27 17:49:17 +0900

    repository.save()

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,14 +1,29 @@
 package repository;
 
+import domain.Product;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 
+@Repository
 public class ProductRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
     public ProductRepository(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void save(Product product) {
+        String sql = "insert into product " +
+                "(name, price, stock_quantity, description) " +
+                "values (?, ?, ?, ?)";
+        jdbcTemplate.update(sql,
+                product.getName(),
+                product.getPrice(),
+                product.getStockQuantity(),
+                product.getDescription()
+        );
     }
 }
