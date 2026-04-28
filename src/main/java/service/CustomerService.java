d2f94a8ab5ed3ee09764d617f14a9e4df456e25b
muzimzz <rlagusdn0914@naver.com>
2026-04-28 14:15:22 +0900

    Service layer 구현

---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,52 @@
+package service;
+
+import domain.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import repository.CustomerRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    
+    private final CustomerRepository customerRepository;
+
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    public void update(Customer customer) {
+        customerRepository.update(customer);
+    }
+
+    public void updatePassword(Long id, String inputPassword, String newPassword) {
+        Customer customer = findById(id);
+        if (customer == null)
+            throw new IllegalArgumentException("존재하지 않는 사용자");
+        if (customer.getPassword().equals(inputPassword)) {
+            customerRepository.updatePassword(customer.getCustomerId(), newPassword);
+        }
+
+        throw new IllegalArgumentException("잘못된 비밀번호");
+
+    }
+
+    public void deleteById(Long id) {
+        if (findById(id) == null)
+            throw new IllegalArgumentException("존재하지 않는 사용자");
+        else customerRepository.deleteById(id);
+    }
+
+
+}
+
---
+++ src/main/java/service/ProductService.java
@@ -0,0 +1,38 @@
+package service;
+
+import domain.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public void update(Product product) {
+        productRepository.update(product);
+    }
+
+    public void deleteById(Long id) {
+        if (findById(id) == null)
+            throw new IllegalArgumentException("존재하지 않는 상품");
+
+        productRepository.deleteById(id);
+    }
+}
