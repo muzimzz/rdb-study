a17c0fdb5e0514a9188e1af895742a3c235db4b5
muzimzz <rlagusdn0914@naver.com>
2026-04-28 17:42:53 +0900

    Request/ResponseDto 클래스 작성

---
+++ src/main/java/com/study/rdb_study/dto/ProductResponse.java
@@ -0,0 +1,29 @@
+package com.study.rdb_study.dto;
+
+import com.study.rdb_study.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // https://developer-anxi.tistory.com/30
+public class ProductResponse {
+    private Long productId;
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String description;
+
+    public static ProductResponse fromEntity(Product product) {
+        return ProductResponse.builder()
+                .productId(product.getProductId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .stockQuantity(product.getStockQuantity())
+                .description(product.getDescription())
+                .build();
+    }
+}
