c36fdbff2561343bce9933431683e6296021444e
muzimzz <rlagusdn0914@naver.com>
2026-04-28 13:27:13 +0900

    JDBC로 CRUD 구현

---
+++ src/main/java/repository/CustomerRepository.java
@@ -0,0 +1,193 @@
+package repository;
+
+import domain.Customer;
+import domain.Order;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CustomerRepository {
+
+    private final DataSource dataSource;
+
+    public CustomerRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void save(Customer customer) {
+        String sql = "insert into customers (name, email, password, address) values (?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, customer.getName());
+            pstmt.setString(2, customer.getEmail());
+            pstmt.setString(3, customer.getPassword());
+            pstmt.setString(4, customer.getAddress());
+
+            pstmt.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    public Customer findById(Long id) {
+        String sql = "select customer_id, name, email, address, join_date from customers where customer_id=?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setLong(1, id);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Customer customer = mapRow(rs);
+                return customer;
+
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    public List<Customer> findAll() {
+        String sql = "select customer_id, name, email, address, join_date from customers";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<Customer> customers = new ArrayList<>();
+            while (rs.next()) {
+                customers.add(mapRow(rs));
+            }
+            return customers;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally  {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    public void update(Customer customer) {
+        String sql = "update customers set email=?, address=? where customer_id=?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, customer.getEmail());
+            pstmt.setString(2, customer.getAddress());
+            pstmt.setLong(3, customer.getCustomerId());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    public void updatePassword(Long id, String newPassword) {
+        String sql = "update customers set password=? where customer_id=?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, newPassword);
+            pstmt.setLong(2, id);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    public void deleteById(Long id) {
+        String sql = "delete from customers where customer_id=?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    private Customer mapRow(ResultSet rs) throws SQLException {
+        Customer customer = new Customer();
+        customer.setCustomerId(rs.getLong("customer_id"));
+        customer.setName(rs.getString("name"));
+        customer.setEmail(rs.getString("email"));
+        customer.setAddress(rs.getString("address"));
+        customer.setJoinDate(rs.getTimestamp("join_date").toLocalDateTime());
+
+        return customer;
+    }
+
+    private void close(Connection conn, Statement pstmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
