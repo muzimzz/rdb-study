ed31ee36cda8cf92e4dd32bf350af74d7eebcc5a
muzimzz <rlagusdn0914@naver.com>
2026-04-28 13:26:55 +0900

    도메인, 스키마 생성

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -1,4 +1,19 @@
 package domain;
 
-public class Member {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private LocalDateTime joinDate;
 }
