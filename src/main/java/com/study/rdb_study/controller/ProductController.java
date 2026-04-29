b15e195447bc608d5a6b9e80508f9f8dc6638272
muzimzz <rlagusdn0914@naver.com>
2026-04-29 15:36:14 +0900

    ProductController 구현(Restful api, dto설계 적용)

--- src/main/java/com/study/rdb_study/controller/ProductController.java
+++ src/main/java/com/study/rdb_study/controller/ProductController.java
@@ -1,8 +1,45 @@
 package com.study.rdb_study.controller;
 
-import org.springframework.stereotype.Controller;
+import com.study.rdb_study.dto.ProductRequest;
+import com.study.rdb_study.dto.ProductResponse;
+import com.study.rdb_study.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public void save(@RequestBody ProductRequest request) {
+        productService.save(request);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponse findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @GetMapping
+    public List<ProductResponse> findAll () {
+        return productService.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id,
+                       @RequestBody ProductRequest request) {
+        productService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
 
 }
