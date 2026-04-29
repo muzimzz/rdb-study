21547d6215d86b39841208459e74fadfc6452db8
muzimzz <rlagusdn0914@naver.com>
2026-04-29 17:51:04 +0900

    controller

--- src/main/java/com/study/rdb_study/controller/OrderController.java
+++ src/main/java/com/study/rdb_study/controller/OrderController.java
@@ -1,8 +1,43 @@
 package com.study.rdb_study.controller;
 
-import org.springframework.stereotype.Controller;
+import com.study.rdb_study.dto.OrderRequest;
+import com.study.rdb_study.dto.OrderResponse;
+import com.study.rdb_study.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("orders")
 public class OrderController {
 
+    private final OrderService orderService;
+
+    @PostMapping
+    public void save(@RequestBody OrderRequest orderRequest) {
+        orderService.save(orderRequest);
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponse findById(@PathVariable Long id) {
+        return orderService.findById(id);
+    }
+
+    @GetMapping
+    public List<OrderResponse> findAll() {
+        return orderService.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id,
+                       @RequestBody OrderRequest orderRequest) {
+        orderService.update(id, orderRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        orderService.deleteById(id);
+    }
 }
