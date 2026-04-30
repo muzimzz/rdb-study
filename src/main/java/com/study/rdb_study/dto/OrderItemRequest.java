ee6ae958c44f5ce9f50f60289a5dddca73b21bec
muzimzz <rlagusdn0914@naver.com>
2026-04-30 15:34:15 +0900

    dto 수정

--- src/main/java/com/study/rdb_study/dto/OrderItemRequest.java
+++ src/main/java/com/study/rdb_study/dto/OrderItemRequest.java
@@ -3,14 +3,15 @@ package com.study.rdb_study.dto;
 import com.study.rdb_study.domain.OrderItem;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class OrderItemRequest {
     private Long orderId;
     private Long productId;
     private int quantity;
 
-    @Builder
     public OrderItem toEntity() {
         return OrderItem.builder()
                 .orderId(this.orderId)
