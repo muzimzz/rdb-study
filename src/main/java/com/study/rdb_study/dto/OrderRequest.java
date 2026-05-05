e522a5bbfe8e6728b98b91866b64c756d9845c54
yoonserin <rlagusdn0914@naver.com>
2026-05-06 00:33:00 +0900

    order-orderItem 로직 추가

--- src/main/java/com/study/rdb_study/dto/OrderRequest.java
+++ src/main/java/com/study/rdb_study/dto/OrderRequest.java
@@ -3,8 +3,10 @@ package com.study.rdb_study.dto;
 import com.study.rdb_study.domain.Order;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -12,10 +14,19 @@ public class OrderRequest {
     private Long customerId;
     private String status;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime orderDate;
+
+    private List<OrderItemRequest> orderItems;
+
     public Order toEntity() {
         return Order.builder()
                 .customerId(this.customerId)
                 .status(this.status)
+                .orderDate(this.orderDate)
+                .orderItems(this.orderItems.stream()
+                        .map(OrderItemRequest::toEntity)
+                        .toList())
                 .build();
     }
 
@@ -24,6 +35,7 @@ public class OrderRequest {
                 .orderId(id)
                 .customerId(this.customerId)
                 .status(this.status)
+                .orderDate(this.orderDate)
                 .build();
     }
 }
