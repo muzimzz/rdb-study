e31041480b6e27691feeea570ff4286238b6d1e6
muzimzz <rlagusdn0914@naver.com>
2026-05-06 12:12:17 +0900

    addLink

--- src/main/java/com/study/rdb_study/global/ErrorResponse.java
+++ src/main/java/com/study/rdb_study/global/ErrorResponse.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 
 // public record ErrorResponse(String message, int status) { }
 
+// https://velog.io/@kiiiyeon/%EC%8A%A4%ED%94%84%EB%A7%81-ExceptionHandler%EB%A5%BC-%ED%86%B5%ED%95%9C-%EC%98%88%EC%99%B8%EC%B2%98%EB%A6%AC
+
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
