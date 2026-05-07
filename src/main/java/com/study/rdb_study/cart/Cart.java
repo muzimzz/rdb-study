354c0a0194f6315f76507ffe2b4daffbf0d89a77
muzimzz <rlagusdn0914@naver.com>
2026-05-07 10:47:54 +0900

    장바구니/장바구니 아이템 도메인 추가

---
+++ src/main/java/com/study/rdb_study/cart/Cart.java
@@ -0,0 +1,24 @@
+package com.study.rdb_study.cart;
+
+import com.study.rdb_study.cartItem.CartItem;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class Cart {
+    private Long id;
+    private Long customerId;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Cart(Long id, Long customerId, LocalDateTime createdAt) {
+        this.id = id;
+        this.customerId = customerId;
+        this.createdAt = createdAt;
+    }
+}
