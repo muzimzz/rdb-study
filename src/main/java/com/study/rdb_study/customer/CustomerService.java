46b17645918a41f411e136ea9bf0475d96bf3af8
muzimzz <rlagusdn0914@naver.com>
2026-05-07 14:33:21 +0900

    누락된 @Transactional 추가

--- src/main/java/com/study/rdb_study/customer/CustomerService.java
+++ src/main/java/com/study/rdb_study/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.study.rdb_study.customer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CustomerService {
     
     private final CustomerRepository customerRepository;
@@ -16,11 +18,13 @@ public class CustomerService {
         return CustomerResponse.toDto(customerRepository.save(customerRequest.toEntity()));
     }
 
+    @Transactional(readOnly = true)
     public CustomerResponse findById(Long id) {
         return CustomerResponse.toDto(customerRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("고객 조회 실패")));
     }
 
+    @Transactional(readOnly = true)
     public List<CustomerResponse> findAll() {
         return customerRepository.findAll().stream()
                 .map(CustomerResponse::toDto)
