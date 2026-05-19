6f40f8e29af89136f64f1c31b7cba35b4686781f
muzi <rlagusdn0914@naver.com>
2026-05-19 15:46:27 +0900

    ProductResponse에서 Quantity, State 삭제

--- src/main/java/com/study/rdb_study/product/ProductResponse.java
+++ src/main/java/com/study/rdb_study/product/ProductResponse.java
@@ -11,18 +11,14 @@ public class ProductResponse {
     private Long productId;
     private String name;
     private int price;
-    private int stockQuantity;
     private String description;
-    private String status;
 
     public static ProductResponse toDto(Product product) {
         return ProductResponse.builder()
                 .productId(product.getProductId())
                 .name(product.getName())
                 .price(product.getPrice())
-                .stockQuantity(product.getStockQuantity())
                 .description(product.getDescription())
-                .status(product.getStatus())
                 .build();
     }
 }
